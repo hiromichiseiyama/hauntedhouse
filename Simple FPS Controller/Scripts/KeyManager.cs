a78f4cfa62d80af26064414825c5583eed1a4ec3
seiyama <seiyama4771@gmail.com>
2020-07-11 12:25:39 +0900

    second commit!!

--- Simple FPS Controller/Scripts/KeyManager.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-
-public static class KeyManager
-{
-    public static KeyCode Jump = KeyCode.Space;
-    public static KeyCode Run = KeyCode.LeftShift;
-    public static KeyCode Grapple = KeyCode.E;
-}
